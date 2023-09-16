9a136069c747e0eba3fd6780dd03a287f8820f59
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-09-16 17:46:21 +0200

    .

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-  constructor(private heroService:HeroService){}
+export class HeroesComponent implements OnInit{
+  
+  heroes:Hero[] = [];
+  constructor(private heroService:HeroService,private messageService:MessageService){}
+  selectedHero?: Hero;
+  
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  onSelect(hero:Hero):void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected Hero id: ${hero.id}`)
+  }
+
   getHeroes():void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
-  ngOnInit():void{
-    this.getHeroes();
-  }
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  heroes:Hero[] = [];
+  
+  
+  
+ 
   
 }
